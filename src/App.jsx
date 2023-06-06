f9d2ac3295cc50dbe854d253afe3939fa43dbd86
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 10:38:33 +0200

    Add Nav, Footer and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './index.css'
+import { Routes, Route } from "react-router-dom"
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 

7bf9b6bb0bf67dfc00014cfee4b10648a4f83298
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 15:15:06 +0200

    Sign in route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Signin from './pages/Signin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Signin />} />
       </Routes>
       <Footer />
     </div>

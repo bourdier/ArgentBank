3926d9a901377ed03a483eae0d4bfbd01369050f
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 15:19:04 +0200

    Fix form

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,23 +1,23 @@
 export default function Signin() {
   return (
     <main className="main bg-dark">
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
+      <section className="form">
+        <i className="fa fa-user-circle form__icon"></i>
         <h1>Sign In</h1>
         <form>
-          <div className="input-wrapper">
+          <div className="input__wrapper">
             <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
-          <div className="input-wrapper">
+          <div className="input__wrapper">
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Remember me</label>
+          <div className="input__remember">
+            <input type="checkbox" id="remember__me" />
+            <label htmlFor="remember__me">Remember me</label>
           </div>
-          <button className="sign-in-button">Sign In</button>
+          <button className="button">Sign In</button>
         </form>
       </section>
     </main>

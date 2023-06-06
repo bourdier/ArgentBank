f12fd21af5e6b5e3889da4e2d2e5b1d9d5031474
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 11:03:19 +0200

    handleLogout function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,42 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Navbar() {
-  const isLoggedIn = false; // Temp value to test the component without having to log in and out
+  const navigate = useNavigate();
+  const isLoggedIn = true; // Temp value to test the component without having to log in and out
+
+  const handleLogout = () => {
+    // Temp function to test the component without having to log in and out
+    console.log("Log out");
+    navigate("/", { replace: true });
+  };
+
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src="./img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
         {isLoggedIn ? (
           <>
-            <a className="main-nav-item" href="./user.html">
+            <Link to="/user" className="main-nav-item">
               <i className="fa fa-user-circle"></i>
               Tony
-            </a>
-            <a className="main-nav-item" href="./index.html">
+            </Link>
+            <Link to="#" className="main-nav-item" onClick={handleLogout}>
               <i className="fa fa-sign-out"></i>
               Sign Out
-            </a>
+            </Link>
           </>
         ) : (
-          <a className="main-nav-item" href="./sign-in.html">
+          <Link to="/login" className="main-nav-item">
             <i className="fa fa-user-circle"></i>
             Sign In
-          </a>
+          </Link>
         )}
       </div>
     </nav>

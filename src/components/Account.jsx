adf79f66f292cf977cfd866f4f12180a263fedd9
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 16:00:10 +0200

    Fix transaction component style

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,13 +1,13 @@
 export default function Account() {
   return (
     <section className="account">
-      <div className="account-content-wrapper">
-        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-        <p className="account-amount">$2,082.79</p>
-        <p className="account-amount-description">Available Balance</p>
+      <div className="account__content__wrapper">
+        <h3 className="account__title">Argent Bank Checking (x8349)</h3>
+        <p className="account__amount">$2,082.79</p>
+        <p className="account__amount__description">Available Balance</p>
       </div>
-      <div className="account-content-wrapper cta">
-        <button className="transaction-button">View transactions</button>
+      <div className="account__content__wrapper cta">
+        <button className="button transaction">View transactions</button>
       </div>
     </section>
   );

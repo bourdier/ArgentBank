bf9b5312541324f984bff394aa6c2c4fb2d40d41
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 11:31:52 +0200

    Add Features' icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import Feature from "../components/Feature";
+import chatIcon from "../assets/images/icon-chat.png";
+import moneyIcon from "../assets/images/icon-money.png";
+import securityIcon from "../assets/images/icon-security.png";
 
 export default function Home() {
   return (
@@ -16,19 +19,19 @@ export default function Home() {
       <section className="features">
         <h2 className="sr-only">Features</h2>
         <Feature
-          imageSrc="images/icon-chat.png"
+          imageSrc={chatIcon}
           imageAlt="Chat Icon"
           title="You are our #1 priority"
           description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
         />
         <Feature
-          imageSrc="images/icon-money.png"
+          imageSrc={moneyIcon}
           imageAlt="Money Icon"
           title="More savings means higher rates"
           description="The more you save with us, the higher your interest rate will be!"
         />
         <Feature
-          imageSrc="images/icon-security.png"
+          imageSrc={securityIcon}
           imageAlt="Security Icon"
           title="Security you can trust"
           description="We use top of the line encryption to make sure your data and money is always safe."

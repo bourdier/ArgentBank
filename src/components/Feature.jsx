fc6266912b8e7bbf9f454793c9badb75a5a7040b
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 10:42:17 +0200

    Add Prop Types

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,6 +1,8 @@
+import PropTypes from 'prop-types';
+
 export default function Feature(props) {
   const { imageSrc, imageAlt, title, description } = props;
-  
+
   return (
     <div className="feature-item">
       <img src={imageSrc} alt={imageAlt} className="feature-icon" />
@@ -11,3 +13,10 @@ export default function Feature(props) {
     </div>
   );
 }
+
+Feature.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};

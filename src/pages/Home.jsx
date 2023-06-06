e7ec562b7d479b191dc8b9b18963622ad2ea786d
Théo Bourdier <theobourdier1@gmail.com>
2023-06-06 10:31:22 +0200

    Add Features section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,24 +12,28 @@ export default function Home() {
           <p className="text">Open a savings account with Argent Bank today!</p>
         </section>
       </div>
-      <Feature
-        imageSrc="images/icon-chat.png"
-        imageAlt="Chat Icon"
-        title="You are our #1 priority"
-        description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-      />
-      <Feature
-        imageSrc="images/icon-money.png"
-        imageAlt="Money Icon"
-        title="More savings means higher rates"
-        description="The more you save with us, the higher your interest rate will be!"
-      />
-      <Feature
-        imageSrc="images/icon-security.png"
-        imageAlt="Security Icon"
-        title="Security you can trust"
-        description="We use top of the line encryption to make sure your data and money is always safe."
-      />
+
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        <Feature
+          imageSrc="images/icon-chat.png"
+          imageAlt="Chat Icon"
+          title="You are our #1 priority"
+          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+        />
+        <Feature
+          imageSrc="images/icon-money.png"
+          imageAlt="Money Icon"
+          title="More savings means higher rates"
+          description="The more you save with us, the higher your interest rate will be!"
+        />
+        <Feature
+          imageSrc="images/icon-security.png"
+          imageAlt="Security Icon"
+          title="Security you can trust"
+          description="We use top of the line encryption to make sure your data and money is always safe."
+        />
+      </section>
     </>
   );
 }

d352ec384345a84b3ba99ddf71a24cf11819b03a
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-08 11:13:23 +0200

    User login to API

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,23 +1,65 @@
+import { useState } from 'react';
+
 export default function Signin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email: username,
+      password: password
+    };
+
+    console.log(formData)
+
+    fetch('http://localhost:3001/api/v1/user/login', {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <main className="main bg-dark">
       <section className="form">
         <i className="fa fa-user-circle form__icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="input__wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="input__wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input__remember">
             <input type="checkbox" id="remember__me" />
             <label htmlFor="remember__me">Remember me</label>
           </div>
-          <button className="button">Sign In</button>
+          <button className="button" type="submit">Sign In</button>
         </form>
       </section>
     </main>

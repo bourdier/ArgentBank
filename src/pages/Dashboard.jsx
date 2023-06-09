580561d1d6eaeff95122ca62de676214c2f3b982
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-09 22:30:17 +0200

    Redirect to login page if user is not logged in

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import Account from "../components/Account";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.usersData.isLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn === false) {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <main className="main bg-dark">
       <div className="header">

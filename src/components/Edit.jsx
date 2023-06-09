33dbde97ffbc90f95c119fa002ad60f177913a13
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-09 23:30:35 +0200

    Edit button navigate to settings page

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Edit() {
   const username = useSelector((state) => state.usersData.username);
   const email = useSelector((state) => state.usersData.email);
+  const navigate = useNavigate();
 
   function splitEmail(email) {
     const first = email[0].toUpperCase();
     const rest = email.slice(1, email.indexOf("@"));
     return first + rest;
+  }
+
+  function settings() {
+    navigate('/settings');
   }
 
   return (
@@ -17,7 +23,9 @@ export default function Edit() {
         <br />
         {username ? username : splitEmail(email)}
       </h1>
-      <button className="button edit">Edit Name</button>
+      <button className="button edit" onClick={() => settings()}>
+        Edit Name
+      </button>
     </>
   );
 }

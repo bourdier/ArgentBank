06b1543e31e86f24a1ca066efb26067aafd8dbb8
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 15:02:47 +0200

    Create Transaction Model for Mongoose

---
+++ server/database/models/transactionModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: String,
+    date: String,
+    description: String,
+    amount: Number,
+    balance: Number,
+    type: String,
+    category: {
+      type: String,
+      enum: ['Electronic', 'Food', 'Rent', 'Transportation', 'Clothing', 'Health', 'Education', 'Entertainment', 'Other']
+    },
+    note: String
+  },
+  {
+    timestamps: true,
+    toObject: {
+      transform: (doc, ret, options) => {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.__v
+        return ret
+      }
+    }
+  }
+)
+
+module.exports = mongoose.model('Transaction', transactionSchema)

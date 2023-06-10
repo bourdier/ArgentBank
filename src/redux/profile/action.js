8f48686d5c2d422e3db2683dddf4861e2f3c37dd
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 12:24:45 +0200

    getProfileUsername

--- src/redux/profile/action.js
+++ src/redux/profile/action.js
@@ -21,3 +21,26 @@ export const setProfileUsername = (token, username) => async (dispatch) => {
     console.log(error);
   }
 };
+
+export const getProfileUsername = (token) => async (dispatch) => {
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+
+    const data = await response.json();
+
+    dispatch(
+      setProfile({
+        username: data.userName,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};

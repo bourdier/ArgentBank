422fe951c8f70aff54251f7252c9101cfa7cc5df
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 21:12:03 +0200

    Use Account component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,7 +26,18 @@ export default function Dashboard() {
         <Edit />
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <Account />
+      <Account 
+        title="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+      />
+      <Account
+        title="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+      />
+      <Account
+        title="Argent Bank Credit Card (x8349)"
+        amount="$184.30"
+      />
     </main>
   );
 }

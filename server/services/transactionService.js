8f52fad7d258aea01b4a032ab1be953e8ce12f0e
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 15:06:46 +0200

    Create Transaction Service

---
+++ server/services/transactionService.js
@@ -0,0 +1,63 @@
+const Transaction = require('../database/models/transactionModel')
+
+module.exports.postTransaction = async serviceData => {
+  try {
+    const newTransaction = new Transaction({
+      userId: serviceData.body.userId,
+      date: serviceData.body.date,
+      description: serviceData.body.description,
+      amount: serviceData.body.amount,
+      balance: serviceData.body.balance,
+      type: serviceData.body.type,
+      category: serviceData.body.category,
+      note: serviceData.body.note
+    })
+
+    let result = await newTransaction.save()
+
+    return result
+  } catch (error) {
+    console.error('Error in transactionService.js', error)
+    throw new Error(error)
+  }
+}
+
+module.exports.updateTransaction = async serviceData => {
+  try {
+    const transaction = await Transaction.findOne({ _id: serviceData.body.id })
+
+    if (!transaction) {
+      throw new Error('Transaction not found!')
+    }
+
+    transaction.date = serviceData.body.date
+    transaction.description = serviceData.body.description
+    transaction.amount = serviceData.body.amount
+    transaction.balance = serviceData.body.balance
+    transaction.type = serviceData.body.type
+    transaction.category = serviceData.body.category
+    transaction.note = serviceData.body.note
+
+    let result = await transaction.save()
+
+    return result
+  } catch (error) {
+    console.error('Error in transactionService.js', error)
+    throw new Error(error)
+  }
+}
+
+module.exports.getTransactions = async serviceData => {
+  try {
+    const transactions = await Transaction.find({ userId: serviceData.query.userId })
+
+    if (!transactions) {
+      throw new Error('Transactions not found!')
+    }
+
+    return transactions
+  } catch (error) {
+    console.error('Error in transactionService.js', error)
+    throw new Error(error)
+  }
+}

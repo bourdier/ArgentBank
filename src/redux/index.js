3e29a4b147056e345dda931b3811a7cc4d8572be
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 12:13:16 +0200

    Remove unused purgeStoredState

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './auth/reducer';
 import { profileSlice } from './profile/reducer';
-import { persistStore, persistReducer, purgeStoredState} from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 

8a576ea21093c4abfa13a0d64203860a30aaf1c4
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 12:05:46 +0200

    Create Profile action

---
+++ src/redux/profile/action.js
@@ -0,0 +1,27 @@
+import { setProfile } from './reducer';
+
+export const setProfileUsername = (token, username) => async (dispatch) => {
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "PUT",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        username: username,
+      }),
+    });
+
+    const data = await response.json();
+
+    dispatch(
+      setProfile({
+        username: username,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};

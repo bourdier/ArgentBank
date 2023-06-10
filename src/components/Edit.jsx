cf6b00bb8b4d043060ba8f591d2c41d8d4c1feca
Théo BOURDIER <theobourdier1@gmail.com>
2023-06-10 13:19:10 +0200

    Add First & last name placeholder

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -7,10 +7,12 @@ export default function Edit() {
   const dispatch = useDispatch();
 
   const username = useSelector((state) => state.profileData.username);
+  const firstname = useSelector((state) => state.profileData.firstname);
+  const lastname = useSelector((state) => state.profileData.lastname);
   const token = useSelector((state) => state.usersData.token);
 
   if (!token) {
-    return
+    return;
   }
 
   function edit() {
@@ -37,20 +39,36 @@ export default function Edit() {
 
       {isOpen && (
         <div className="modal">
-          <button className="button close" onClick={() => setIsOpen(false)}>
-            x
-          </button>
           <form className="modal__content" onSubmit={handleFormSubmit}>
             <div className="input__wrapper">
-              <label htmlFor="username">New username</label>
+              <label htmlFor="username">User name</label>
               <input
                 type="text"
                 id="username"
                 className="input__wrapper"
                 required="required"
               />
+              <label htmlFor="firstname">First name</label>
+              <input
+                type="text"
+                id="firstname"
+                className="input__wrapper"
+                readOnly="readonly"
+                value={firstname}
+              />
+              <label htmlFor="lastname">Last name</label>
+              <input
+                type="text"
+                id="lastname"
+                className="input__wrapper"
+                readOnly="readonly"
+                value={lastname}
+              />
               <button className="button edit" type="submit">
                 Save
+              </button>
+              <button className="button edit" onClick={() => setIsOpen(false)}>
+                Cancel
               </button>
             </div>
           </form>
